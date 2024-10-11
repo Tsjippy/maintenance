e5da341d370b8eab69af22591f5223c158422811
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:21:36 +0100

    added: MODULE_PATH constant

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -3,6 +3,9 @@ namespace SIM\MAINTENANCE;
 use SIM;
 
 const MODULE_VERSION		= '7.0.0';
+
+DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
+
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
