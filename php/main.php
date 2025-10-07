962942e774e07f8deaba2107420c2cc2ae3288c0
Tsjippy <ewaldha@hotmail.com>
2025-10-07 09:56:02 +0100

    changed: classnames

--- php/main.php
+++ php/main.php
@@ -10,7 +10,7 @@ function header(){
         if(!$message){
             $message	= 'This website is currently unavailable, but will be available again soon';
         }
-        $url        = wp_get_attachment_url(SIM\getModuleOption('maintenance', 'picture_ids')['image']);
+        $url        = wp_get_attachment_url(SIM\getModuleOption('maintenance', 'picture-ids')['image']);
         wp_die("<h1>$title</h1><br><img src='$url' alt='' width='400' height='150' style='margin-left:auto;margin-right:auto;display:block;'><br>$message");
     }
 }
