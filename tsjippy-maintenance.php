57d3e51098afc2d5d0535d6a46e926cf40ee331b
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:43 +0200

    changed: removed 'tested up to'

--- tsjippy-maintenance.php
+++ tsjippy-maintenance.php
@@ -10,7 +10,6 @@ namespace TSJIPPY\MAINTENANCE;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
  * Plugin URI:            https://github.com/Tsjippy/maintenance
  * Tested:               7.1
  * TextDomain:            tsjippy
